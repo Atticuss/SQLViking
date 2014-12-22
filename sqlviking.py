293c828c6b4c6421081e8bd1e5134cbfd9e5b09e
Atticuss <jonn.callahan@gmail.com>
2014-12-22 15:16:30 -0500

    parsing functionalty more robust

--- sqlviking.py
+++ sqlviking.py
@@ -11,10 +11,29 @@ import sqlserver
 pkts=Queue()
 queries=Queue()
 
-MYSQLREQ    = 1
-MYSQLRESP   = 2
-SQLSERVREQ  = 3
-SQLSERVRESP = 4
+REQUEST     = 1
+RESPONSE    = 2
+MYSQL       = 10
+MYSQLREQ    = 11
+MYSQLRESP   = 12
+SQLSERV     = 20
+SQLSERVREQ  = 21
+SQLSERVRESP = 22
+UNKNOWN     = 99
+
+class Conn():
+    def __init__(self,cip,cport,db):
+        self.cip = cip
+        self.cport = cport
+        self.db = db
+
+class DataBase():
+    def __init__(self,ip,port,name=UNKNOWN,dbtype):
+        self.ip = ip
+        self.port = port
+        self.name = name
+        self.dbtype = dbtype
+        self.traffic = []
 
 class Parse(threading.Thread):
     #TODO: need to be able to set MTU from cmdline
@@ -24,18 +43,51 @@ class Parse(threading.Thread):
         self.mtu = mtu
         self.frag = {}
         self.res = ''
+        self.knownConns = []
+        self.knownDBs = []
+        self.knownDBServs = {}
 
     def run(self):
         global pkts
         while not self.die:
             if not pkts.empty():
-                self.parse(pkts.get())
+                self.handle(pkts.get())
+
+    def parse(self,pkt,db=None):
+        pktType = UNKNOWN
+        if db:
+            if db.type == SQLSERV:
+                if pkt[IP].src == db.ip:
+                    pktType = SQLSERVRESP
+                else:
+                    pktType = SQLSERVREQ
+            elif db.type == MYSQL:
+                if pkt[IP].src == db.ip:
+                    pktType = MYSQLRESP
+                else:
+                    pktType = MYSQLREQ
+
+        if pktType != UNKNOWN:
+            pktType = self.isMySql(pkt)
+        elif pktType != UNKNOWN:
+            pktType = self.isSqlServ(pkt)
+
+        if pktType == MYSQLREQ:
+            self.parseMySqlReq(pkt,db)
+        elif pktType == MYSQLRESP:
+            self.parseMySqlResp(pkt,db)
+        elif pktType == SQLSERVREQ:
+            self.parseSqlServReq(pkt,db)
+        elif pktType == SQLSERVRESP:
+            self.parseSqlServResp(pkt,db)
+
+        return pktType
 
     def isMySql(self, pkt):
         pktlen = len(pkt)/2
         lengths=[]
         while len(pkt)>0:
-            length = int(flipEndian(pkt[:6]),16)
+            length = int(self.flipEndian(pkt[:6]),16)
             lengths.append(length)
             pkt = pkt[8+(length*2):]
 
@@ -45,47 +97,140 @@ class Parse(threading.Thread):
         tlen+= len(lengths)*4
 
         if tlen == pktlen:
-            return True
+            if self.isMySqlReq(pkt):
+                return MYSQLREQ
+            else:
+                return MYSQLRESP
         else:
-            return False
+            return UNKNOWN
 
-    def flipEndian(data):
+    def isMySqlReq(self,pkt):    
+        return True
+
+    def isMySqlResp(self,pkt):
+        return True
+
+    def isSqlServ(self,pkt):
+        return UNKNOWN
+
+    def isSqlServReq(self,pkt):
+        return False
+
+    def isSqlServResp(self,pkt):
+        return False
+
+    def flipEndian(self,data):
         resp=''
         for i in range(0,len(data),2):
             resp = data[i]+data[i+1]+resp
         return resp
 
-    def parse(self,pkt):
+    def inConn(self,c,pkt):
+        if c.cip == pkt[IP].dst and c.cport == pkt[TCP].dport and c.db.ip == pkt[IP].src and c.db.port == pkt[TCP].sport:
+            return RESPONSE
+        elif c.cip == pkt[IP].src and c.cport == pkt[TCP].sport and c.db.ip == pkt[IP].dst and c.db.port == pkt[TCP].dport:
+            return REQUEST
+
+    def getConn(self,pkt):
+        for c in self.knownConns:
+            if self.inConn(c,pkt):
+                return c
+
+    def removeConn(self,pkt):
+        for c in self.knownConns:
+            if self.isConn(c,pkt):
+                del self.knownConns[self.knownConns.index(c)]
+                break
+
+    def isKnownDB(self,pkt):
+        for db in self.knownDBs:
+            if pkt[IP].dst == db.ip and pkt[TCP].dport == dp.port:
+                return db
+            if pkt[IP].src == db.ip and pkt[TCP].sport == db.port:
+                return db
+
+    def findDB(self,ip,port):
+        for db in self.knownDBs:
+            if ip == db.ip and port == db.port:
+                return db
+
+    def addConn(self,pkt,db):
+        if pkt[IP].dst == db.port:
+            c = Conn(pkt[IP].src,pkt[TCP].sport,db)
+        else:
+            c = Conn(pkt[IP].dst,pkt[TCP].dport,db)
+        self.knownConns.append(c)
+
+    def handle(self,pkt):
+        #pkt in knownConn?
+        c = self.getConn(pkt)
+        if c:
+            #pkt have fin/ack?
+            if pkt[TCP].flags == "FA":
+                #remove from knownConns; break
+                del self.knownConns[self.knownConns.index(c)]
+                return
+            #parse accordingly; break
+            self.parse(pkt,c.db)
+            return
+        
+        #pkt to/from knownDB?
+        db = self.isKnownDB(pkt)
+        if db:
+            self.parse(pkt,db)
+        #pkt a sqlserv/mysql req?
+        pktType = self.parse(pkt)
+        if pktType == MYSQLRESP:
+            ip = pkt[IP].src
+            port = pkt[TCP].sport
+            dbtype = MYSQL
+        elif pktType == MYSQLREQ:
+            ip = pkt[IP].dst
+            port = pkt[TCP].dport
+            dbtype = MySQL
+        elif pktType == SQLSERVRESP:
+            ip = pkt[IP].src
+            port = pkt[TCP].sport
+            dbtype = SQLSERV
+        elif pktType == SQLSERVREQ:
+            ip = pkt[IP].dst
+            port = pkt[TCP].dport
+            dbtype = SQLSERV
+
+        if ip and port and dbtype:
+            db = DataBase(ip=ip,port=port,dbtype=dbtype)
+
+    #def parse(self,pkt):
         #TODO: determining parser by port. need to account for DBs on non-standard ports.
         #print '\nSource:\t%s\nTCP Val:\t%s\nAck:\t%s\nSeq:\t%s\n'%(pkt[IP].src,str(pkt[TCP]).encode('hex'),pkt[TCP].ack,pkt[TCP].seq)
-        if pkt[TCP].sport == 1433 or pkt[TCP].sport == 3306:
+        #if pkt[TCP].sport == 1433 or pkt[TCP].sport == 3306:
             #reassesmble pkts if fragged
-            key='%s:%s'%(pkt[IP].dst,pkt[TCP].dport)
-            if len(str(pkt[IP])) == self.mtu:
-                try:
-                    self.frag[key]+=str(pkt[TCP])[20:]
-                except KeyError:
-                    self.frag[key]=str(pkt[TCP])[20:]
-            else:
-                try:
-                    if pkt[TCP].sport == 1433:
-                        self.parseRespSQLServ(self.frag[key]+str(pkt[TCP])[20:])
-                    else:
-                        self.parseRespMySQL(self.frag[key]+str(pkt[TCP])[20:])
-                    del self.frag[key]
-                except KeyError:
-                    if pkt[TCP].sport == 1433:
-                        self.parseRespSQLServ(str(pkt[TCP])[20:])
-                    else:
-                        self.parseRespMySQL(str(pkt[TCP])[20:])
-        elif pkt[TCP].dport == 1433:
+        #    key='%s:%s'%(pkt[IP].dst,pkt[TCP].dport)
+        #    if len(str(pkt[IP])) == self.mtu:
+        #        try:
+        #            self.frag[key]+=str(pkt[TCP])[20:]
+        #        except KeyError:
+        #            self.frag[key]=str(pkt[TCP])[20:]
+        #    else:
+        #        try:
+        #            if pkt[TCP].sport == 1433:
+        #                self.parseSqlServResp(self.frag[key]+str(pkt[TCP])[20:])
+        #            else:
+        #                self.parseMySqlResp(self.frag[key]+str(pkt[TCP])[20:])
+        #            del self.frag[key]
+        #        except KeyError:
+        #            if pkt[TCP].sport == 1433:
+        #                self.parseSqlServResp(str(pkt[TCP])[20:])
+        #            else:
+        #                self.parseMySqlResp(str(pkt[TCP])[20:])
+        #elif pkt[TCP].dport == 1433:
             #Pillage POC
             #if len(pkt[TCP]) == 26:
             #    req = sqlserver.Request()
             #    send(IP(dst="192.168.37.135",src=pkt[IP].src)/TCP(flags="PA",dport=pkt[TCP].dport,sport=pkt[TCP].sport,seq=pkt[TCP].seq,ack=pkt[TCP].ack)/req.buildRequest("select top 1 * from customerLogin"))
-            self.parseReqSQLServ(str(pkt[TCP]).encode('hex')[40:])
-        elif pkt[TCP].dport == 3306:
-            self.parseReqMySQL(str(pkt[TCP]).encode('hex')[40:])
+        #    self.parseSqlServReq(str(pkt[TCP]).encode('hex')[40:])
+        #elif pkt[TCP].dport == 3306:
+        #    self.parseMySqlReq(str(pkt[TCP]).encode('hex')[40:])
 
     def validAscii(self,h):
         if int(h,16)>31 and int(h,16)<127:
@@ -105,12 +250,12 @@ class Parse(threading.Thread):
             res+="%s, "%i
         return res[:-2]
 
-    def parseReqMySQL(self,data):
+    def parseMySqlReq(self,data):
         self.logres("\n--MySQL Req--\n")
         self.logres("Raw: %s\n"%data)
         self.logres("ASCII: %s\n"%self.readable(data))
 
-    def parseRespMySQL(self,data):
+    def parseMySqlResp(self,data):
         self.logres("\n--MySQL Resp--\n")
         res = connections.MySQLResult(connections.Result(data))
         try:
@@ -126,10 +271,10 @@ class Parse(threading.Thread):
             self.logres('[!] Error:\t%s\n'%sys.exc_info()[1])
         self.logres('[*] Raw:\t%s\n'%str(data).encode('hex'))
 
-    def parseReqSQLServ(self,data):
+    def parseSqlServReq(self,data):
         self.logres("\n--SQLServ Req--\n%s\n"%self.readable(data))
 
-    def parseRespSQLServ(self,data):
+    def parseSqlServResp(self,data):
         resp = sqlserver.Response(data)
         resp.parse()
         
