de2e1b2d3f95560550a031459028ca4c155d4c57
relotnek <ken@kentoler.com>
2015-01-07 20:07:48 -0500

    mysql fix and note to fix bind address

--- vagrant-scripts/setup-weakapp.sh
+++ vagrant-scripts/setup-weakapp.sh
@@ -4,17 +4,17 @@ sudo apt-get install git -y
 sudo apt-get install unzip -y
 
 mkdir /opt/weakapp
-cp -R /vagrant/weakapp/* /opt/app
+cp -R /vagrant/sinatra-app/* /opt/app
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /home/vagrant/.rvm/scripts/rvm
-rvm install 1.9.2
+rvm install 2.0.0
 
 sudo apt-get install freetds-dev freetds-bin tdsodbc libmysqlclient-dev
 
-
 cd /opt/app
-git clone https://github.com/fidalgo/ruby-mssql-example
+bundle install
+ruby app.rb
 
 
