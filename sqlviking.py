cb141af1576528e8d00fac0919b9f178ba522e0b
Atticuss <Atticuss@github.com>
2015-04-19 15:44:11 -0400

    initial config file functionality finished, no sanity checks

--- sqlviking.py
+++ sqlviking.py
@@ -6,8 +6,10 @@ sys.path.append("databases/")
 from constantvalues import *
 import mysql,sqlserver
 
-dbs = Queue()
-toInject = Queue()
+dbQueue = Queue()
+injectionQueue = Queue()
+
+debug = True
 
 class Conn():
     def __init__(self,cip,cport,sip,sport,state,db,nextcseq=-1,nextsseq=-1):
@@ -22,7 +24,7 @@ class Conn():
         self.nextcseq = nextcseq
         self.nextsseq = nextsseq
 
-class DataBase():
+class Database():
     def __init__(self,dbType,ip,port):
         self.ip       = ip
         self.port     = port
@@ -33,12 +35,14 @@ class DataBase():
         self.schemas  = []
 
     def getHumanType(self):
-        if ISMYSQL(self.dbType):
-            return 'MySQL'
-        elif ISSQLSERV(self.dbType):
-            return 'SQL Server'
-        else:
-            return 'Unknown'
+        return self.dbType
+        #dprint(self.dbType)
+        #if ISMYSQL(self.dbType):
+        #    return 'MySQL'
+        #elif ISSQLSERV(self.dbType):
+        #    return 'SQL Server'
+        #else:
+        #    return 'Unknown'
 
     def addUser(self,u):
         if u not in self.users:
@@ -62,14 +66,23 @@ class Scout(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.die = False
+        self.toInject = []
         self.knownDatabases = []
 
     def run(self):
         global dbs
         while not self.die:
-            if not dbs.empty():
-                self.knownDatabases.append(dbs.get())
-                print '[*] DB found: %s at %s:%s'%(self.knownDatabases[-1].getHumanType(),self.knownDatabases[-1].ip,self.knownDatabases[-1].port)
+            if not dbQueue.empty():
+                self.knownDatabases.append(dbQueue.get())
+                #dprint('[?] DB found: %s at %s:%s'%(self.knownDatabases[-1].getHumanType(),self.knownDatabases[-1].ip,self.knownDatabases[-1].port))
+            if not injectionQueue.empty():
+                self.toInject.append(injectionQueue.get())
+                #dprint('[?] Injection parsed - %s'%self.toInject[-1])
+
+def dprint(s):
+    global debug
+    if debug:
+        print(s)
 
 def parseConfig(f):
     global dbs,toInject
@@ -81,37 +94,32 @@ def parseConfig(f):
     flag = -1
     with open(f,'r') as f:
         for l in f:
-            if len(l.strip()) == 0:
-                flag = -1
-            elif l == '[Databases]':
+            l = l.strip()
+            if len(l) == 0 or l[0] == '#':
+                continue #donothing
+            elif '[Databases]' in l:
                 flag = DATABASES
-            elif l == '[Injection]':
+            elif '[Injection]' in l:
                 flag = INJECTION
-            elif l == '[Data]':
+            elif '[Data]' in l:
                 flag = DATA
-            elif l == '[Misc]':
+            elif '[Misc]' in l:
                 flag = MISC
-
-            if flag == DATABASES:
+            elif flag == DATABASES:
                 l = l.split(':')
-                dbs.put(Database(l[0],l[1],l[2]))
+                #dprint('[?] Parsing line for db info:\t%s'%l)
+                dbQueue.put(Database(l[0],l[1],l[2]))
             elif flag == INJECTION:
-                toInject.put(l)
+                injectionQueue.put(l)
             elif flag == DATA:
                 l = l #do nothing for know, leave check so i remember to update later when stuff is actually implemented
             elif flag == MISC:
                 l = l #do nothing for know, leave check so i remember to update later when stuff is actually implemented
 
 def main():
-    #t1 = Scout()
-    #t2 = Parse()
-    #t1.start()
-    #t2.start()
-
     parser = argparse.ArgumentParser(description='Pwn the network, pwn the database', prog='sqlviking.py', usage='%(prog)s [-v] -c <config file location>', formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=65, width =150))
     parser.add_argument('-v', '--verbose', action='store_true', help='Turn on verbose mode; will print out all captured traffic')
     parser.add_argument('-c', '--configfile', default='sqlviking.conf', help='Config file location, defaults to sqlviking.conf')
-    parser.add_argument('-h', '--help', help='Display help info about using sqlviking')
     args = parser.parse_args()
 
     try:
@@ -120,21 +128,24 @@ def main():
         print('[!] Error reading config file. Exiting...')
         sys.exit(0)
 
-    while True:
-        #printMainMenu(t2)
-        try:
+    t1 = Scout()
+    #t2 = Parse()
+    t1.start()
+    #t2.start()
+
+    #printMainMenu(t2)
+    try:
+        while True:
             a = 1
-            #nonBlockingRawInput(t2)
-        except KeyboardInterrupt:
-            print('\n[!] Shutting down...')
-            t1.die = True
-            #t2.die = True
-            break
-        except:
-            t1.die = True
-            #t2.die = True
-            print sys.exc_info()[1]
-            break
+        #nonBlockingRawInput(t2)
+    except KeyboardInterrupt:
+        print('\n[!] Shutting down...')
+        t1.die = True
+        #t2.die = True
+    except:
+        t1.die = True
+        #t2.die = True
+        print sys.exc_info()[1]
 
 if __name__ == "__main__":
     main()
